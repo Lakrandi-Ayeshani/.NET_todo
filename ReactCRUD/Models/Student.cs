70197c8b344ce3241e0eb08d74c2304c783a3503
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2024-05-14 13:03:08 +0530

    all end points and data fields are implemented

---
+++ ReactCRUD/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReactCRUD.Models
+{
+    public class Student
+    {
+        [Key]
+        public int id { get; set; }
+        public string studentname { get; set; }
+
+        public string course { get; set; }
+    }
+}
---
+++ ReactCRUD/Models/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReactCRUD.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+    }
+}
